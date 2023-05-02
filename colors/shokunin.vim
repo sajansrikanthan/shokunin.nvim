10d0a1201df23871a541924ee20b1263d4c092d8
Sajan Srikanthan <70323293+sajansrikanthan@users.noreply.github.com>
2023-05-02 22:14:38 +0200

    init

---
+++ colors/shokunin.vim
@@ -0,0 +1,4 @@
+lua << EOF
+local shokunin = require("shokunin")
+shokunin.setup({})
+EOF
---
+++ lua/shokunin/palette.lua
@@ -0,0 +1,18 @@
+local colors = {
+  fg = "#FFFFFF",
+  bg = "#1E1E1E",
+  orange1 = "#DB8530",
+  orange2 = "#D6492E",
+  purple = "#6A42F6",
+  green1 = "#42C732",
+  green2 = "#39ED2A",
+  magenta1 = "#E44CE2",
+  magenta2 = "#C11489",
+  cyan = "#41C4D1",
+  red = "#E60000",
+  visualline = "#264F78",
+  cursor_fg = "#000000",
+  cursor_bg = "#9D9D9D",
+}
+
+return colors
