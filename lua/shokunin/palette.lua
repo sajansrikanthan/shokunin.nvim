eb4001593d08b6c09a58812e27f1a504d90c387f
Sajan Srikanthan <70323293+sajansrikanthan@users.noreply.github.com>
2023-05-03 19:37:16 +0200

    update

--- lua/shokunin/palette.lua
+++ lua/shokunin/palette.lua
@@ -10,6 +10,7 @@ local colors = {
   magenta2 = "#C11489",
   cyan = "#41C4D1",
   red = "#E60000",
+  blue = "#569CD6",
   visualline = "#264F78",
   cursor_fg = "#000000",
   cursor_bg = "#9D9D9D",
